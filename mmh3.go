d6e6460f93c1fce0e194639b3aae41710b5af743
Jason Moiron <jmoiron@jmoiron.net>
2014-08-15 21:10:57 +0000

    make mix constants actual constants, add unsafe x64 optimized hash

--- mmh3.go
+++ mmh3.go
@@ -3,6 +3,15 @@ package mmh3
 import (
 	"bytes"
 	"encoding/binary"
+	"reflect"
+	"unsafe"
+)
+
+const (
+	h32c1 uint32 = 0xcc9e2d51
+	h32c2 uint32 = 0x1b873593
+	h64c1 uint64 = 0x87c37b91114253d5
+	h64c2 uint64 = 0x4cf5ad432745937f
 )
 
 func Hash32(key []byte) uint32 {
@@ -10,15 +19,14 @@ func Hash32(key []byte) uint32 {
 	if length == 0 {
 		return 0
 	}
-	var c1, c2 uint32 = 0xcc9e2d51, 0x1b873593
 	nblocks := length / 4
 	var h, k uint32
 	buf := bytes.NewBuffer(key)
 	for i := 0; i < nblocks; i++ {
 		binary.Read(buf, binary.LittleEndian, &k)
-		k *= c1
+		k *= h32c1
 		k = (k << 15) | (k >> (32 - 15))
-		k *= c2
+		k *= h32c1
 		h ^= k
 		h = (h << 13) | (h >> (32 - 13))
 		h = (h * 5) + 0xe6546b64
@@ -34,9 +42,9 @@ func Hash32(key []byte) uint32 {
 		fallthrough
 	case 1:
 		k ^= uint32(key[tailIndex])
-		k *= c1
+		k *= h32c1
 		k = (k << 15) | (k >> (32 - 15))
-		k *= c2
+		k *= h32c2
 		h ^= k
 	}
 	h ^= uint32(length)
@@ -56,23 +64,22 @@ func Hash128(key []byte) []byte {
 		binary.Write(ret, binary.LittleEndian, uint64(0))
 		return ret.Bytes()
 	}
-	var c1, c2 uint64 = 0x87c37b91114253d5, 0x4cf5ad432745937f
 	nblocks := length / 16
 	var h1, h2, k1, k2 uint64
 	buf := bytes.NewBuffer(key)
 	for i := 0; i < nblocks; i++ {
 		binary.Read(buf, binary.LittleEndian, &k1)
 		binary.Read(buf, binary.LittleEndian, &k2)
-		k1 *= c1
+		k1 *= h64c1
 		k1 = (k1 << 31) | (k1 >> (64 - 31))
-		k1 *= c2
+		k1 *= h64c2
 		h1 ^= k1
 		h1 = (h1 << 27) | (h1 >> (64 - 27))
 		h1 += h2
 		h1 = h1*5 + 0x52dce729
-		k2 *= c2
+		k2 *= h64c2
 		k2 = (k2 << 33) | (k2 >> (64 - 33))
-		k2 *= c1
+		k2 *= h64c1
 		h2 ^= k2
 		h2 = (h2 << 31) | (h2 >> (64 - 31))
 		h2 += h1
@@ -101,9 +108,9 @@ func Hash128(key []byte) []byte {
 		fallthrough
 	case 9:
 		k2 ^= uint64(key[tailIndex+8])
-		k2 *= c2
+		k2 *= h64c2
 		k2 = (k2 << 33) | (k2 >> (64 - 33))
-		k2 *= c1
+		k2 *= h64c1
 		h2 ^= k2
 		fallthrough
 	case 8:
@@ -129,9 +136,9 @@ func Hash128(key []byte) []byte {
 		fallthrough
 	case 1:
 		k1 ^= uint64(key[tailIndex])
-		k1 *= c1
+		k1 *= h64c1
 		k1 = (k1 << 31) | (k1 >> (64 - 31))
-		k1 *= c2
+		k1 *= h64c2
 		h1 ^= k1
 	}
 	h1 ^= uint64(length)
@@ -153,4 +160,122 @@ func Hash128(key []byte) []byte {
 	binary.Write(ret, binary.LittleEndian, h1)
 	binary.Write(ret, binary.LittleEndian, h2)
 	return ret.Bytes()
+}
+
+// Hash128x64 is a version of MurmurHash which is designed to run only on
+// little-endian processors.  It is considerably faster for those processors
+// than Hash128.
+func Hash128x64(key []byte) []byte {
+	length := len(key)
+	ret := make([]byte, 16)
+	if length == 0 {
+		return ret
+	}
+
+	rh := *(*reflect.SliceHeader)(unsafe.Pointer(&ret))
+
+	nblocks := length / 16
+	var h1, h2, k1, k2 uint64
+
+	h := *(*reflect.SliceHeader)(unsafe.Pointer(&key))
+	h.Len = nblocks * 2
+	b := *(*[]uint64)(unsafe.Pointer(&h))
+	for i := 0; i < len(b); i += 2 {
+		k1, k2 = b[i], b[i+1]
+		k1 *= h64c1
+		k1 = (k1 << 31) | (k1 >> (64 - 31))
+		k1 *= h64c2
+		h1 ^= k1
+		h1 = (h1 << 27) | (h1 >> (64 - 27))
+		h1 += h2
+		h1 = h1*5 + 0x52dce729
+		k2 *= h64c2
+		k2 = (k2 << 33) | (k2 >> (64 - 33))
+		k2 *= h64c1
+		h2 ^= k2
+		h2 = (h2 << 31) | (h2 >> (64 - 31))
+		h2 += h1
+		h2 = h2*5 + 0x38495ab5
+	}
+	h.Len = length
+
+	k1, k2 = 0, 0
+	tailIndex := nblocks * 16
+	switch length & 15 {
+	case 15:
+		k2 ^= uint64(key[tailIndex+14]) << 48
+		fallthrough
+	case 14:
+		k2 ^= uint64(key[tailIndex+13]) << 40
+		fallthrough
+	case 13:
+		k2 ^= uint64(key[tailIndex+12]) << 32
+		fallthrough
+	case 12:
+		k2 ^= uint64(key[tailIndex+11]) << 24
+		fallthrough
+	case 11:
+		k2 ^= uint64(key[tailIndex+10]) << 16
+		fallthrough
+	case 10:
+		k2 ^= uint64(key[tailIndex+9]) << 8
+		fallthrough
+	case 9:
+		k2 ^= uint64(key[tailIndex+8])
+		k2 *= h64c2
+		k2 = (k2 << 33) | (k2 >> (64 - 33))
+		k2 *= h64c1
+		h2 ^= k2
+		fallthrough
+	case 8:
+		k1 ^= uint64(key[tailIndex+7]) << 56
+		fallthrough
+	case 7:
+		k1 ^= uint64(key[tailIndex+6]) << 48
+		fallthrough
+	case 6:
+		k1 ^= uint64(key[tailIndex+5]) << 40
+		fallthrough
+	case 5:
+		k1 ^= uint64(key[tailIndex+4]) << 32
+		fallthrough
+	case 4:
+		k1 ^= uint64(key[tailIndex+3]) << 24
+		fallthrough
+	case 3:
+		k1 ^= uint64(key[tailIndex+2]) << 16
+		fallthrough
+	case 2:
+		k1 ^= uint64(key[tailIndex+1]) << 8
+		fallthrough
+	case 1:
+		k1 ^= uint64(key[tailIndex])
+		k1 *= h64c1
+		k1 = (k1 << 31) | (k1 >> (64 - 31))
+		k1 *= h64c2
+		h1 ^= k1
+	}
+	h1 ^= uint64(length)
+	h2 ^= uint64(length)
+	h1 += h2
+	h2 += h1
+	h1 ^= h1 >> 33
+	h1 *= 0xff51afd7ed558ccd
+	h1 ^= h1 >> 33
+	h1 *= 0xc4ceb9fe1a85ec53
+	h1 ^= h1 >> 33
+	h2 ^= h2 >> 33
+	h2 *= 0xff51afd7ed558ccd
+	h2 ^= h2 >> 33
+	h2 *= 0xc4ceb9fe1a85ec53
+	h2 ^= h2 >> 33
+	h1 += h2
+	h2 += h1
+
+	rh.Len = 2
+	b = *(*[]uint64)(unsafe.Pointer(&rh))
+	b[0] = h1
+	b[1] = h2
+	rh.Len = 16
+	return ret
 }
